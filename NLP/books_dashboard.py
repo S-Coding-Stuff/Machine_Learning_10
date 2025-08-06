82657ab9f0c386fcc0be4853e7a6ae844184061a
S-Coding-Stuff <sam.grant.info@gmail.com>
2025-08-06 09:48:56 +0100

    Updated Dashboard

--- NLP/books_dashboard.py
+++ NLP/books_dashboard.py
@@ -18,3 +18,11 @@ raw_documents = TextLoader('tagged_description.txt').load()
 text_splitter = CharacterTextSplitter(separator='\n', chunk_size=1000, chunk_overlap=0)
 documents = text_splitter.split_documents(raw_documents)
 db_books = Chroma.from_documents(documents, OpenAIEmbeddings())
+
+def retrieve_semantic_recommendations(query, category, tone, initial_top_k=50, final_top_k=16):
+    recommendations = db_books.similarity_search(query, k=initial_top_k)
+    books_list = [int(recommendation.page_content.strip('"').split()[0]) for recommendation in recommendations]
+    book_recs = books[books['isbn13'].isin(books_list).head(final_top_k)]
+
+    if category != 'All':
+        book_recs = book_recs[book_recs['categories'] == category]
